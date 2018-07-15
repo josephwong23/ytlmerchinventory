a5241c01f8ebc8da40e72ce7f4e7b02acc956518
Liren <liren90@gmail.com>
2018-07-16 02:52:59 +0800

    Secret sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   belongs_to :member_division
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { registration: 'yltcc'}
   resources :orders
   resources :members
   resources :users
